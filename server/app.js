ecb57c2efeb997252fcc526e76695a93f6096dff
Linus Romland <romlandlinus@gmail.com>
2020-12-09 12:55:00 +0100

    NOT WORKING

--- server/app.js
+++ server/app.js
@@ -1,41 +1,46 @@
-const { parse } = require("node-html-parser");
 const fetch = require("node-fetch");
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 const fs = require("fs");
+const AbortController = require('abort-controller');
+
+
 
 if (!fs.existsSync("links.json"))
   fs.writeFileSync("links.json", '{"links":[]}');
 let linksFile = JSON.parse(fs.readFileSync("./links.json"));
 
 //START URL
-let currentURL = "https://www.romland.space/";
+let startUrl = "https://www.inet.se/";
 
 main();
 
 //Main function, needed async
 async function main() {
-  let dom = new JSDOM(await fetchLink(currentURL));
-  let links = dom.window.document.links;
-
-  addsOrUpdatesLink(currentURL);
-
-  for (let i = 0; i < links.length; i++) {
-    if (links[i].href.startsWith("https")) {
-      if (!checkIfExist(links[i].href)) {
-        writeToJson({
-          link: links[i].href,
-          visited: false,
-        });
-      }
+  while (linksFile.links.length < 1000) {
+    if (!linksFile.links[0]) {
+      await addLinksFromURL(startUrl);
+    } else {
+      await linksFile.links.forEach(async (link) => {
+        if (!link.visited) {
+          await addLinksFromURL(link.link);
+        }
+      });
     }
-    linksFile = JSON.parse(fs.readFileSync("./links.json"));
   }
 }
 
 //Fetches from link using node-fetch
 async function fetchLink(link) {
-  let tmp = await fetch(link);
+  const controller = new AbortController();
+  const timeout = setTimeout(
+    () => { controller.abort(); },
+    500,
+  );
+   
+  let tmp = await fetch(link, { signal: controller.signal });
+  clearTimeout(timeout);
+  console.log(tmp)
   return await tmp.text();
 }
 
@@ -68,6 +73,23 @@ function addsOrUpdatesLink(url) {
       if (tmp.links[i].link == url) tmp.links[i].visited = true;
     }
     fs.writeFileSync("links.json", JSON.stringify(tmp), "utf8");
+    linksFile = JSON.parse(fs.readFileSync("./links.json"));
+  }
+}
+
+async function addLinksFromURL(currentURL) {
+  let dom = new JSDOM(await fetchLink(currentURL));
+  let links = dom.window.document.links;
+  addsOrUpdatesLink(currentURL);
+  for (let i = 0; i < links.length; i++) {
+    if (links[i].href.startsWith("https")) {
+      if (!checkIfExist(links[i].href)) {
+        writeToJson({
+          link: links[i].href,
+          visited: false,
+        });
+      }
+    }
     linksFile = JSON.parse(fs.readFileSync("./links.json"));
   }
 }
