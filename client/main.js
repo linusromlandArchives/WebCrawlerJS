010c7628d33834751cba8bbe3eb190bc657adbb5
superfroggman <49985901+superfroggman@users.noreply.github.com>
2020-12-09 18:07:39 +0100

    added client going to random links

--- client/main.js
+++ client/main.js
@@ -1,6 +1,17 @@
+let linksFile;
+async function onLoad() {
+  let promise = await fetch("./links.json");
+  linksFile = JSON.parse(await promise.text());
+  
+  setInterval(changeLink, timePerSite);
+}
 
-setInterval(changeLink(), 2000)
+let timePerSite = 2000
 
-function changeLink(){
-    console.log("change link here")
+function changeLink() {
+  console.log("change link here");
+  let rand = Math.floor(Math.random() * linksFile.links.length);
+  console.log(linksFile.links[rand].link);
+
+  document.getElementById("frame").src = linksFile.links[rand].link
 }
