6ecbf26344d648268421aebe96b8b95205b6fae3
Linus Romland <romlandlinus@gmail.com>
2020-12-10 15:53:07 +0100

    Revert "Merge pull request #3 from linusromland/mongo-connect"
    This reverts commit e2292ab9e12d0a042e5227180e6d0b3f48c375b0, reversing
    changes made to bae423460d55e61903bda99ee25de016ec7a940d.

--- server/app.js
+++ server/app.js
@@ -3,7 +3,6 @@ const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 const fs = require("fs");
 const AbortController = require("abort-controller");
-const dBModule = require("./dbModule.js");
 let deadEnd = false;
 
 if (!fs.existsSync("links.json")) {
@@ -21,9 +20,6 @@ if (process.argv[2]) {
 
 //START URL
 let startUrl = "https://en.wikipedia.org/wiki/Linus";
-
-//Connect to MongoDB
-connectToMongo("WebCrawler");
 
 if (process.argv[3]) {
   startUrl = process.argv[3];
@@ -163,13 +159,5 @@ async function addLinksFromURL(currentURL) {
       }
       linksFile = JSON.parse(fs.readFileSync("./links.json"));
     }
-  }
-}
-
-function connectToMongo(dbName) {
-  if (fs.existsSync("mongoauth.json")) {
-    dBModule.cnctDBAuth(dbName);
-  } else {
-    dBModule.cnctDB(dbName);
   }
 }
